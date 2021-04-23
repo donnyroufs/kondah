ce239b80132e25cdc5fbc77e0b8e3dbafbca689c
Donny Roufs <iamchets@yahoo.com>
2021-04-23 18:41:40 +0200

    cleanup

--- apps/example-api/src/app/application.ts
+++ apps/example-api/src/app/application.ts
@@ -1,10 +1,9 @@
 import 'reflect-metadata'
-import { Konda, KondaContext, IOC, RouteDefinition } from '@konda/core'
+import { Konda, KondaContext, IOC } from '@konda/core'
 import { UserService } from './user.service'
 import { AuthService } from './auth.service'
 import { NestedService } from './nested.service'
 
-import { AppController as controller } from './app.controller'
 import { Request, Response } from 'express'
 
 import './app.controller'
@@ -24,18 +23,17 @@ export class Application extends Konda {
   }
 
   public async setup(context: KondaContext) {
-    // const u = context.ioc.get(UserService)
-    // // Test nested dep
-    // console.log(u.getNested())
-    // // Check if singleton dep works
-    // u.addUser()
-    // u.addUser()
-    // console.log({ get1: u.getUsers() })
-    // const u2 = context.ioc.get(UserService)
-    // console.log({ get2: u2.getUsers() })
-    // context.ioc.rebind(UserService.name, UserService)
-    // const u3 = context.ioc.get(UserService)
-    // console.log({ get3: u3.getUsers() }) // should be empty
-    // Iterate over all our controllers and register our routes
+    const u = context.ioc.get(UserService)
+    // Test nested dep
+    console.log(u.getNested())
+    // Check if singleton dep works
+    u.addUser()
+    u.addUser()
+    console.log({ get1: u.getUsers() })
+    const u2 = context.ioc.get(UserService)
+    console.log({ get2: u2.getUsers() })
+    context.ioc.rebind(UserService.name, UserService)
+    const u3 = context.ioc.get(UserService)
+    console.log({ get3: u3.getUsers() }) // should be empty
   }
 }
