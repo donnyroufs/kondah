c95a5adc9353eed161aa2650b89c96eee1bf87f5
Donny Roufs <iamchets@yahoo.com>
2021-04-23 14:36:19 +0200

    eslint ignore

--- libs/core/src/lib/injectable.decorator.ts
+++ libs/core/src/lib/injectable.decorator.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata'
-import { ioc } from './ioc'
 
 export function injectable() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return function (target: any) {
     const injectables = Reflect.getMetadata('design:paramtypes', target)
     target.prototype.__injectables__ = injectables
