c9bada055563eaea46f218944bd61cbeceaa820c
Donny Roufs <iamchets@yahoo.com>
2021-04-23 15:53:56 +0200

    obsolete type

--- libs/core/src/lib/types.ts
+++ libs/core/src/lib/types.ts
@@ -1,6 +1,6 @@
 import { ServerAdapter } from './server-adapter'
 import { Plugin } from './plugin'
-import { IOC as _IOC } from './ioc'
+import { IOC } from './ioc'
 
 export interface IKondaContext {
   server: Omit<ServerAdapter, 'run'>
@@ -11,8 +11,6 @@ export interface IKondaOptions {
   server: ServerAdapter
   plugins?: Plugin[]
 }
-
-export type IOC = _IOC
 
 export type Constructor<T> = new (...args: unknown[]) => T
 export type Scopes = 'transient' | 'singleton'
