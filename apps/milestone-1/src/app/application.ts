c7ff60b4584d9e7666706ffcb19fb174dbcd03e9
Donny Roufs <iamchets@yahoo.com>
2021-04-26 01:52:11 +0200

    konda*

--- apps/milestone-1/src/app/application.ts
+++ apps/milestone-1/src/app/application.ts
@@ -1,5 +1,5 @@
 import { AppContext, Energizor, Konda } from '@konda/core'
-import { ExpressAdapter } from '@koda/express-adapter'
+import { ExpressAdapter } from '@konda/express-adapter'
 
 export class Application extends Konda {
   protected async configureServices(services: Energizor) {}
