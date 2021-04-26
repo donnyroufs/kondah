25866811a627d3e2438232e3838647ae7f4fe208
Donny Roufs <iamchets@yahoo.com>
2021-04-26 16:06:16 +0200

    add info comment

--- apps/milestone-1/src/main.ts
+++ apps/milestone-1/src/main.ts
@@ -2,6 +2,8 @@ import { Application } from './app/application'
 import { ExpressAdapter } from '@konda/express-adapter'
 import { HttpControllerPlugin } from '@konda/http-controller'
 
+// Temporarly exporting because there's no proper way to
+// inject the AppContext right now.
 export const app = new Application({
   server: new ExpressAdapter(),
   plugins: [HttpControllerPlugin],
