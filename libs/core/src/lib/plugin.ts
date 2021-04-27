92e75baf8af4a240a3270f581d747676163e351b
Donny Roufs <iamchets@yahoo.com>
2021-04-27 22:51:57 +0200

    remove unknown type

--- libs/core/src/lib/plugin.ts
+++ libs/core/src/lib/plugin.ts
@@ -1,15 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { AppContext } from './app.context'
 
 export abstract class Plugin {
   public abstract name: string
 
-  constructor(private readonly _config: unknown) {}
+  constructor(private readonly _config: any) {}
 
   public async install(context: AppContext) {
     await this.setup(context, this.getConfig())
   }
 
-  protected abstract setup(context: AppContext, config: unknown): Promise<void>
+  protected abstract setup(context: AppContext, config: any): Promise<void>
 
   private getConfig() {
     return this._config[this.name]
