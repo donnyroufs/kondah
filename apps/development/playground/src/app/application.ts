0c9b27404af7b2ed552ef01e1b3767ac9dd0b7e8
Donny Roufs <iamchets@yahoo.com>
2021-04-28 11:28:49 +0200

    add notes

--- apps/development/playground/src/app/application.ts
+++ apps/development/playground/src/app/application.ts
@@ -1,12 +1,69 @@
-import { AppContext, Energizor, Kondah } from '@kondah/core'
+import { AppContext, Energizor, Injectable, Kondah } from '@kondah/core'
 import { ExpressAdapter } from '@kondah/express-adapter'
+import { Controller, Get } from '@kondah/http-controller'
+import { Person } from '../main'
+
+@Injectable()
+export class DatabaseService {
+  findUser() {
+    return { id: 1, name: 'donny' }
+  }
+}
+
+@Injectable()
+class UserService {
+  constructor(
+    // private readonly _person: Person,
+    private readonly _appContext: AppContext
+  ) {}
+
+  doSomething() {
+    //@ts-expect-error yes
+    console.log(this._appContext.authenticate())
+    // this._person.greet()
+  }
+}
+
+@Controller('/')
+class AppController {
+  // constructor(private readonly _userService: UserService) {}
+  constructor(private readonly _appContext: AppContext) {}
+
+  @Get('/')
+  index(req, res) {
+    // res.json(this._userService.doSomething())
+    // @ts-expect-error Yes
+    res.json(this._appContext.authenticate())
+  }
+}
 
 export class Application extends Kondah {
   protected async configureServices(services: Energizor) {
     services.setDefaultScope('singleton')
+
+    services.register(UserService)
+    services.register(DatabaseService)
+  }
+
+  /**
+   * Gives us access to all the installed plugins before we run Kondah Setup.
+   * Some plugins might require some kind of implementation that we do not expose by default
+   * like our database. So in here we can provide it.
+   */
+  protected async preInstall(currentPlugins: any) {
+    // add the required functionality?
+    currentPlugins.required = [() => null]
   }
 
   protected async setup(ctx: AppContext<ExpressAdapter>) {
+    // console.log(ctx.energizor)
+    // ctx.energizor.get(UserService)
+    console.log(ctx.energizor.get(Person).greet())
+    ctx.energizor.get(UserService).doSomething()
+
+    // ctx.on('serialize-user', () => database.findOne(...))
+    // ctx.on('deserialize-user', (user) => user.id)
+
     ctx.server.run(5000)
   }
 }
--- apps/development/playground/src/main.ts
+++ apps/development/playground/src/main.ts
@@ -1,8 +1,58 @@
-import { Application } from './app/application'
+import { Application, DatabaseService } from './app/application'
 import { ExpressAdapter } from '@kondah/express-adapter'
+import {
+  AddToContext,
+  AppContext,
+  IAppConfig,
+  Plugin,
+  ServerAdapter,
+} from '@kondah/core'
+import { HttpControllerPlugin } from '@kondah/http-controller'
+
+export class Person {
+  greet() {
+    return 'hey'
+  }
+}
+
+export class StupidPlugin extends Plugin {
+  public name = 'stupid-plugin'
+
+  protected async setup(context: AppContext<ServerAdapter>) {
+    context.energizor.register(Person)
+  }
+
+  @AddToContext()
+  authenticate() {
+    return 'authenticated'
+  }
+}
+
+export class AuthPlugin extends Plugin {
+  public name = 'auth-plugin'
+
+  protected async setup(
+    context: AppContext<ServerAdapter>,
+    config: IAppConfig
+  ) {
+    console.log('installing auth plugin')
+
+    // We can get things from the dependency container here! Since plugins run AFTER we register all our services
+    // console.log('exists:', !!context.energizor.get('UserService'))
+
+    // if(config.driver === 'prisma') {
+    //   const db = context.energizor.get(DatabaseService)
+    //   const user = db.findUser()
+    // }
+  }
+}
 
 new Application({
   server: new ExpressAdapter(),
-  plugins: [],
-  config: {},
+  plugins: [StupidPlugin, HttpControllerPlugin, AuthPlugin],
+  config: {
+    'http-controller': {
+      serveRoutes: true,
+    },
+  },
 })
