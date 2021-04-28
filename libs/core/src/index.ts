88018bbdee7930fdf02c63d346d9ffcc4ddd65c8
Donny Roufs <iamchets@yahoo.com>
2021-04-28 14:18:53 +0200

    Solve issue where it does not merge config types correctly.
    Co-authored-by: Daniell <daniellwdb@users.noreply.github.com>

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -6,6 +6,10 @@ export * from './lib/injectable.decorator'
 export * from './lib/add-to-context.decorator'
 export * from './lib/metadata.types'
 
+// Re-export interface to allow declaration merging
+// without this line, `export * as` above only exposes this type as type alias
+export { IAppConfig } from './lib/types'
+
 // TODO: When we have @Inject don't expose the context anymore
 export { AppContext } from './lib/app.context'
 
