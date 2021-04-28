0e3949741fcfabc37021700c781353666155c7c6
Donny Roufs <iamchets@yahoo.com>
2021-04-28 12:51:05 +0200

    protected config inside plugin

--- libs/core/src/lib/plugin.ts
+++ libs/core/src/lib/plugin.ts
@@ -4,7 +4,7 @@ import { AppContext } from './app.context'
 export abstract class Plugin {
   public abstract name: string
 
-  constructor(private readonly _config: any) {}
+  constructor(protected readonly _config: any) {}
 
   public async install(context: AppContext) {
     await this.setup(context, this.getConfig())
