4c7ea9b96dfead59d10b62d900e21e0561f3a1d9
Donny Roufs <iamchets@yahoo.com>
2021-05-01 14:22:06 +0200

    use token

--- packages/core/src/dependency-data.ts
+++ packages/core/src/dependency-data.ts
@@ -5,6 +5,11 @@ export class DependencyData<T> {
 
   constructor(
     public scope: Scopes,
+    /**
+     * used to get the dependency. Could be either the constructor name,
+     * or a custom token (string, symbol)
+     */
+    public token: string | symbol,
     public dependency: Dependency<T>,
     injectables: Dependency[],
     public cache: null | T = null
