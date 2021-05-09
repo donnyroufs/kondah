ef35353c4b3ce817d7ced5994464925610b5d102
Donny Roufs <iamchets@yahoo.com>
2021-05-09 18:24:20 +0200

    fix template

--- packages/cli/templates/controller-context/src/application.ts
+++ packages/cli/templates/controller-context/src/application.ts
@@ -1,11 +1,11 @@
 import { AppContext, Energizor, Kondah } from '@kondah/core'
 
-import './controllers'
-
 export class Application extends Kondah {
   protected async configureServices(services: Energizor) {}
   protected async setup(ctx: AppContext) {
     ctx.welcome()
+    ctx.addControllers()
+
     ctx.server.run(5000)
   }
 }
