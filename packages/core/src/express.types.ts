fb16edafda00eeb0c7df92b38d2e3e6f45eb8673
Donny Roufs <iamchets@yahoo.com>
2021-07-09 16:15:15 +0200

    add httpcontext types

--- packages/core/src/express.types.ts
+++ packages/core/src/express.types.ts
@@ -2,7 +2,7 @@ import express = require('express')
 import { IKondahRequestData } from './types'
 
 /*
-	Dirty fix for now, because I'm not sure why it's not wrking as intenteded
+	Dirty fix for now, because I'm not sure why it's not working as intended
 	when included as a .d.ts file.
 */
 declare module 'express' {
--- packages/http-context/src/@types/index.ts
+++ packages/http-context/src/@types/index.ts
@@ -1,6 +1,12 @@
+import { HttpContext } from '../http.context'
+
 declare module '@kondah/core' {
   interface AppContext {
     addToHttpContext<T>(key: string, fn: T): void
+  }
+
+  interface IKondahRequestData {
+    httpContext: HttpContext
   }
 }
 
