903da58094e609c2812999d7edcf4e39f967f74b
Donny Roufs <iamchets@yahoo.com>
2022-01-08 21:30:09 +0100

    feat(core): expose energizor imports in core
    This prevents the need of having to install 2 packages just to use Kondah.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,13 @@
 export * from './kondah'
 export * from './types'
 export * from './logger'
+
+export {
+  ExcludeHooks,
+  IEnergizor,
+  Inject,
+  Injectable,
+  TestableEnergizor,
+  ICollection,
+  IBoot,
+} from '@kondah/energizor'
