22624b73771005ed95ebf99c640deb9506c115a8
Donny Roufs <iamchets@yahoo.com>
2022-01-08 19:14:57 +0100

    feat: expose energizor in core
    This prevents the user from having to install multiple dependencies.
    They are still free to use Energizor without kondah/core.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,13 @@
 export * from './kondah'
 export * from './types'
 export * from './logger'
+
+export {
+  IEnergizor,
+  ICollection,
+  TestableEnergizor,
+  Injectable,
+  Inject,
+  IBoot,
+  ExcludeHooks,
+} from '@kondah/energizor'
