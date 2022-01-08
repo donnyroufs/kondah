1a0d0a5d9cf184818f3d5500584916a8baf8fb8d
Donny Roufs <iamchets@yahoo.com>
2022-01-08 19:08:23 +0100

    feat(energizor): register mocks for testing
    In order to use test doubles we need a way to register them without
    TypeScript being mad at us.

--- packages/energizor/src/test-utils/testable-energizor.ts
+++ packages/energizor/src/test-utils/testable-energizor.ts
@@ -1,13 +1,15 @@
 import { Energizor } from '../energizor'
 import { MockedLogger } from './mocked-logger'
+import { CanNotRebindUnknownDependencyException } from '../exceptions/can-not-rebind-unknown-dependency.exception'
+import { Utils } from '../utils'
+import { AbstractPckg } from '../packages/abstract-pckg'
 import {
   ICollection,
   ILogger,
   Constructor,
-  DependencyConstr,
   DepOrToken,
   IRebind,
-  Package,
+  DependencyConstr,
 } from '../types'
 
 export class TestableEnergizor extends Energizor implements IRebind {
@@ -20,11 +22,27 @@ export class TestableEnergizor extends Energizor implements IRebind {
     this.collections = collections
   }
 
-  public rebind<T>(
-    depOrToken: DepOrToken<T>,
-    constr: DependencyConstr<T>,
-    type: Package = Package.SINGLETON
-  ) {
-    this.register(type, depOrToken, constr)
+  public rebind<T>(depOrToken: DepOrToken<T>, constr: unknown) {
+    const registry = this.getRegistry()
+    const pckg = registry.get(depOrToken)
+
+    if (!pckg) {
+      throw new CanNotRebindUnknownDependencyException(depOrToken)
+    }
+
+    this.flagByConstructorProp(pckg, constr)
+
+    registry.set(depOrToken, pckg)
+  }
+
+  private flagByConstructorProp(pckg: AbstractPckg, constr: unknown) {
+    const isConstructor = Utils.isConstructor(constr)
+
+    if (!isConstructor) {
+      pckg.getDependency().flagAsConstantValue(constr)
+      return
+    }
+
+    pckg.getDependency().setConstructor(constr as DependencyConstr)
   }
 }
